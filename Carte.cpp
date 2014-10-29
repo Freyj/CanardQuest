0bb63926d847b5f988bd75c36ae250578560a7ef
Freyj <aerandil@gmail.com>
2014-10-29 15:45:26 +0100

    creation de carte

--- Carte.cpp
+++ Carte.cpp
@@ -1,5 +1,6 @@
 #include "Carte.hpp"
 #include <iostream>
+#include <fstream>
 
 Carte::Carte()
 {
@@ -13,5 +14,47 @@ Carte::~Carte()
 
 void Carte::creerCarte()
 {
-	
+	std::ifstream f ("carte");
+	char a;
+
+	//petit message d'erreur en cas d'absence de fichier
+	if (!f.is_open())
+		{
+			std::cerr << "Impossible d'ouvrir le fichier en lecture !" << std::endl;
+	  	}
+	//sinon, on lit
+	else
+	{	
+		int cpt = 0;
+		//tant qu'il y a à lire & qu'on a pas atteint le bout de la carte
+		while(!f.eof() && (cpt <256))
+		{
+			f.get(a);
+			//comme get récupère un char, 0 = 48 en ascii
+			this->setTab(cpt, (a-48));
+			++cpt;
+		}
+
+		f.close();
+	}	
+}
+
+void Carte::setTab(int a, int b)
+{
+	if ((0 <= a) && (a < 256))
+	{
+		this->tab_[a] = b;
+	}
+
+
+}
+//avec une petite vérification, si le chiffre n'est pas compris 
+//dans les bonnes valeurs, -1
+int Carte::getTab(int i)
+{
+	if ((0 <= i) && (i < 256))
+	{
+		return this->tab_[i];
+	}
+	return -1;
 }
--- Carte.hpp
+++ Carte.hpp
@@ -1,6 +1,7 @@
 #ifndef CARTE_HPP
 #define CARTE_HPP
 
+#include <string>
 /**
 Classe qui gère la carte du jeu représentée par un tableau 
 d'entiers pour le moment (un entier représente un type de terrain)
@@ -17,6 +18,15 @@ public:
 	méthode qui va créer la carte à partir d'un fichier
 	*/
 	void creerCarte();
+	/**
+	méthode qui renvoit un string pour savoir quoi afficher ?
+	*/
+	std::string sendAff();
+
+	//permet de mettre un entier donné (b)sur la case voulue(a)
+	void setTab(int a, int b);
+	//permet d'accéder à l'entier dans une case voulue
+	int getTab(int i);
 
 };
 #endif
