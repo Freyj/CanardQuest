866aa5c41cce2f21208bbc837ebf381290c1cf1d
Freyj <aerandil@gmail.com>
2014-11-30 18:30:44 +0100

    commentaires sur renard

--- Renard.hpp
+++ Renard.hpp
@@ -6,6 +6,8 @@
 #include "Predateur.hpp"
 #include "Canard.hpp"
 
+/**La classe Renard hérite de Prédateur et représente un prédateur terrestre capable de manger le canard et de marcher uniquement là où il n'y a pas d'eau ni d'obstacles */
+
 class Renard:public Predateur
 {
 	public:
